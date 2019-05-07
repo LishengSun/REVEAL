a50aba02176d5b81fd4639d5009e7687970296a2
lishengsun <cecile829@gmail.com>
2019-05-07 18:28:37 +0200

    Added original MNIST setting and DQN trained from scratch (without pretrained CNN)

---
+++ agents/DQN/context.py
@@ -0,0 +1,7 @@
+import os
+import sys
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '../../env/')))
+
+# from env import MNIST_env
+# import env.MNIST_env
+import MNIST_env
