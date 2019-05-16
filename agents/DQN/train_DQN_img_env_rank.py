corrected the push to memory and the computation of Q

agents/DQN/train_DQN_img_env_rank.py
@@ -19,7 +19,6 @@ from MNIST_env import img_env_rank
 
 import utils
 import seaborn as sns
-import pdb
 
 
 import model
@@ -34,8 +33,26 @@ import os, time
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
 
+def generate_colors(n):
+	# https://www.quora.com/How-do-I-generate-n-visually-distinct-RGB-colours-in-Python
+	ret = []
+	r = int(random.random() * 256)
+	g = int(random.random() * 256)
+	b = int(random.random() * 256)
+	step = 256 / n
+	for i in range(n):
+		r += step
+		g += step
+		b += step
+		r = int(r) % 256
+		g = int(g) % 256
+		b = int(b) % 256
+		ret.append('#%02x%02x%02x' % (r, g, b))
+	return ret
 
-def smoothing_average(x, factor=100):
+
+
+def smoothing_average(x, factor=500):
 	running_x = 0
 	X = copy.deepcopy(x)
 	for i in range(len(X)):
@@ -62,15 +79,13 @@ class myNet(nn.Module):
 
 		if action_space.__class__.__name__ == "Discrete": # our case
 			num_outputs = action_space.n
-			self.dist = Categorical(self.base.output_size+action_space.n, num_outputs)
+			self.dist = Categorical(self.base.output_size, num_outputs)
 		elif action_space.__class__.__name__ == "Box":
 			num_outputs = action_space.shape[0]
 			self.dist = DiagGaussian(self.base.output_size, num_outputs)
 		else:
 			raise NotImplementedError
 
-		if dataset in ['mnist', 'cifar10']:
-			self.clf = Categorical(self.base.output_size, action_space.n) 
 
 		self.state_size = self.base.state_size
 
@@ -81,21 +96,9 @@ class myNet(nn.Module):
 		actor_features, states = self.base(inputs, states, masks)
 		self.actor_features = actor_features
 
-		if self.dataset in img_env_rank.IMG_ENVS:
-			clf = self.clf(self.actor_features)
-			clf_proba = clf.probs
-
-			# classif = clf_proba.sort()[-1][0][-3:] # -1=idx, 0=first example -3:=3 highest proba
-
-			if deterministic:
-				classif = clf.mode()
-			else:
-				classif = clf.sample()
-			
-
-		actor_features_with_clf_proba = torch.cat((actor_features, clf_proba), 1) 
-		dist = self.dist(actor_features_with_clf_proba)
-		Q_values = dist.logits
+		Q_values, dist = self.dist(actor_features)
+		# pdb.set_trace()
+		# Q_values = dist.logits
 		if deterministic:
 			action = dist.mode()
 		else:
@@ -103,9 +106,8 @@ class myNet(nn.Module):
 
 		action_log_probs = dist.log_probs(action)
 
-		action = torch.cat([action, classif], 1)
 
-		return action, Q_values, clf_proba, states #dist.logits = Q values
+		return action, Q_values, states #dist.logits = Q values
 
 
 
@@ -130,11 +132,11 @@ class ReplayMemory(object):
 		return len(self.memory)
 
 Transition = namedtuple('Transition',
-						('state', 'action', 'next_state', 'reward', 'target'))
+						('state', 'action', 'next_state', 'reward'))
 
 
 
-def optimize_myNet(net, optimizer, BATCH_SIZE=128, OPTIMIZE_CLF=False):
+def optimize_myNet(net, optimizer, BATCH_SIZE=128):
 	if len(memory) < BATCH_SIZE:
 		return
 	print ('Optimizing')
@@ -145,6 +147,8 @@ def optimize_myNet(net, optimizer, BATCH_SIZE=128, OPTIMIZE_CLF=False):
 	non_final_mask = torch.tensor(tuple(map(lambda s: s is not None,
 										  batch.next_state)), dtype=torch.uint8).to(device)
 
+	# print ('non_final_mask', non_final_mask)
+
 	non_final_next_states = torch.stack([s for s in batch.next_state \
 		if s is not None]).to(device)
 	# print ('non_final_next_states', non_final_next_states.shape)
@@ -152,9 +156,10 @@ def optimize_myNet(net, optimizer, BATCH_SIZE=128, OPTIMIZE_CLF=False):
 	
 	action_batch = torch.stack(batch.action).to(device)
 	reward_batch = torch.cat(batch.reward).to(device)
+	# print ('reward_batch', reward_batch)
 
 	
-	_, Q_values_batch, clf_proba_batch, _ = net.act(
+	_, Q_values_batch, _ = net.act(
 		inputs=state_batch.float(),
 		states=state_batch, masks=state_batch[1])
 	
@@ -164,28 +169,17 @@ def optimize_myNet(net, optimizer, BATCH_SIZE=128, OPTIMIZE_CLF=False):
 
 	next_state_values = torch.zeros(BATCH_SIZE).to(device)
 
-	_, next_Q_values_batch, _, _= target_net.act(\
+	_, next_Q_values_batch, _= target_net.act(\
 		inputs=non_final_next_states.float(),\
 		states=non_final_next_states, masks=non_final_next_states[1])
-	
 	
 	next_state_values[non_final_mask] = next_Q_values_batch.max(1)[0].detach()
 	
 	expected_state_action_values = next_state_values * GAMMA + reward_batch # Compute the expected Q values
 
-
 	loss_navig = F.smooth_l1_loss(state_action_values, expected_state_action_values.unsqueeze(1))
 	
-	# target_prob_batch = torch.stack(batch.target_prob).to(device)
-	target_batch = torch.cat(batch.target).to(device)
-	
-	loss_clf = F.nll_loss(torch.log(clf_proba_batch), target_batch)
-	# pdb.set_trace()
-	if OPTIMIZE_CLF:
-		total_loss = 0.1*loss_clf + 0.9*loss_navig
-	else:
-		total_loss = loss_navig  #only train on navigation loss
-	# total_loss = 0.1*loss_clf + 0.9*loss_navig
+	total_loss = loss_navig  #only train on navigation loss
 
 	optimizer.zero_grad()
 	total_loss.backward()
@@ -199,34 +193,23 @@ def optimize_myNet(net, optimizer, BATCH_SIZE=128, OPTIMIZE_CLF=False):
 	optimizer.step()
 
 	# print ('total_loss, loss_clf, loss_navig', total_loss.detach().item(), loss_clf.detach().item(), loss_navig.detach().item())
-	return total_loss, loss_clf, loss_navig
+	return total_loss
 
 
 
 ####################### TRAINING ############################
 if __name__ == '__main__':
 
-	# Collect arguments 
-	parser = optparse.OptionParser()
-	parser.add_option('--OPTIMIZE_CLF', action="store_true", \
-		dest="OPTIMIZE_CLF", help='whether to also optimize the classification loss')
-
-	
-	parser.set_defaults(OPTIMIZE_CLF=False)
-
-	options, args = parser.parse_args()
-
-	OPTIMIZE_CLF = options.OPTIMIZE_CLF
-
-
-
 	BATCH_SIZE = 128
 	NUM_STEPS = 49
 	GAMMA = 1 - (1 / NUM_STEPS) # Set to horizon of max episode length
-	EPS = 0.05
+	
 	NUM_LABELS = 2
 	WINDOW_SIZE = 5
-	NUM_EPISODES = 10000
+	NUM_EPISODES = 1000
+	EPS = 0.2
+	EPS_annealing_rate = (EPS-0.05) / NUM_EPISODES # annealed to 0.05 at the end of episodes
+
 	TARGET_UPDATE = 10
 	RUNS = 1
 	MODEL_DIR = './trained_model/'
@@ -238,7 +221,7 @@ if __name__ == '__main__':
 	
 	run_durations = []
 	run_total_rewards = []
-	run_loss_clf = []
+	run_loss = []
 	for run in range(RUNS):
 		net = myNet(\
 			obs_shape=env.observation_space.shape, \
@@ -252,51 +235,74 @@ if __name__ == '__main__':
 
 		total_rewards = []
 		episode_durations = []
-		loss_classification = []
+		loss = []
 		q_values = []
 		optimizer = optim.Adam(filter(lambda p: p.requires_grad, net.parameters()), lr=0.0001, amsgrad=True)
+		q_distribution_checkpoints = int(NUM_EPISODES / 10)
+		q_colors = generate_colors(NUM_EPISODES//q_distribution_checkpoints)
 
 		for i_episode in range(NUM_EPISODES):
-			print ('run %i, episode %i'%(run, i_episode))
 			total_reward_i = 0
 			observation = env.reset()
-			target = env.target
+			EPS -= EPS_annealing_rate
+			print ('run %i, episode %i, exploration eps=%f'%(run, i_episode, EPS))
 
 			for t in range(NUM_STEPS): # allow 100 steps
-				actionS, Q_values, clf_proba, states = net.act(inputs=torch.from_numpy(observation).float().resize_(1, 2, 32, 32).to(device), \
+				action, Q_values, states = net.act(inputs=torch.from_numpy(observation).float().resize_(1, 2, 32, 32).to(device), \
 					states=observation, masks=observation[1])
-				actionS = actionS.cpu().numpy()[0]
-				patch_pred = actionS[1]
-				last_observation = observation
+				action = action.cpu().numpy()[0]
+				current_state = observation
 				rand = np.random.rand()
 				if rand < EPS:
-					actionS = np.array(
-						[np.random.choice(range(env.action_space.n)), np.random.choice(range(env.action_space.n))])
-				action = actionS[0]
-				observation, reward, done, info = env.step(actionS)
+					action = np.array([np.random.choice(range(env.action_space.n))])
+				observation, reward, done, info = env.step(action[0])
 				total_reward_i = reward + GAMMA*total_reward_i
-				memory.push(torch.from_numpy(last_observation), torch.from_numpy(actionS), \
-					torch.from_numpy(observation), torch.tensor([reward]).float(), torch.tensor([target]))
-				optimize_myNet(net, optimizer, BATCH_SIZE, OPTIMIZE_CLF)
+				if not done:
+					next_state = observation
+					# print ('next_state = 1')
+					memory.push(torch.from_numpy(current_state), torch.from_numpy(action), \
+					torch.from_numpy(next_state), torch.tensor([reward]).float())
+				else:
+					next_state = None
+					# print ('next_state', next_state)
+					memory.push(torch.from_numpy(current_state), torch.from_numpy(action), \
+					next_state, torch.tensor([reward]).float())
+				
+				loss_i = optimize_myNet(net, optimizer, BATCH_SIZE)
 
 				if done:
-				# print ('Done after %i steps'%(t+1))
+					# print ('Done after %i steps, reward = %f'%(t+1, total_reward_i))
 					break
+			print ('After %i steps, reward = %f, Done=%s'%(t+1, total_reward_i,str(done)))
+			
 				# Update the target network, copying all weights and biases in DQN
 			if i_episode % TARGET_UPDATE == 0:
 				target_net.load_state_dict(net.state_dict())
 
-			loss_classification_i = F.nll_loss(torch.log(clf_proba), torch.tensor([env.target]).to(device))
 			total_rewards.append(total_reward_i)
+			try:
+				loss.append(loss_i.item())
+			except Exception:
+				pass
 			episode_durations.append(t+1)
-			loss_classification.append(loss_classification_i.detach().item())
 			q_values.append(Q_values)
+
+			if (i_episode+1) % q_distribution_checkpoints == 0:
+				plt.plot(Q_values.cpu().detach().numpy()[0], c=q_colors[(i_episode+1)//q_distribution_checkpoints-1], label='episode %i'%i_episode)
+				# print (q_colors[(i_episode+1)//1000-1])
+				# print ()
+				# print ()
+				plt.title('Q value distributions')
+				plt.legend()
+				plt.savefig(os.path.join(RESULT_DIR,'Q_annealingExp_combLoss_img_env_rank_{NUM_LABELS}labs_{RUNS}runs_{NUM_EPISODES}epis_{NUM_STEPS}steps_{WINDOW_SIZE}ws'.format(**locals())))
+				
+
 		run_durations.append(episode_durations)
 		run_total_rewards.append(total_rewards)
-		run_loss_clf.append(loss_classification)
+		run_loss.append(loss)
 
-	torch.save(net.state_dict(), os.path.join(MODEL_DIR,'model_img_env_rank_OPT_CLF{OPTIMIZE_CLF}_{NUM_LABELS}labs_{RUNS}runs_{NUM_EPISODES}epis_{NUM_STEPS}steps_{WINDOW_SIZE}ws.pth'.format(**locals())))
-	torch.save(optimizer.state_dict(), os.path.join(MODEL_DIR,'optimizer_img_env_rank_OPT_CLF{OPTIMIZE_CLF}_{NUM_LABELS}labs_{RUNS}runs_{NUM_EPISODES}epis_{NUM_STEPS}steps_{WINDOW_SIZE}ws.pth'.format(**locals())))
+	torch.save(net.state_dict(), os.path.join(MODEL_DIR,'annealingExp_combLoss_model_img_env_rank_{NUM_LABELS}labs_{RUNS}runs_{NUM_EPISODES}epis_{NUM_STEPS}steps_{WINDOW_SIZE}ws.pth'.format(**locals())))
+	torch.save(optimizer.state_dict(), os.path.join(MODEL_DIR,'annealingExp_combLoss_optimizer_img_env_rank_{NUM_LABELS}labs_{RUNS}runs_{NUM_EPISODES}epis_{NUM_STEPS}steps_{WINDOW_SIZE}ws.pth'.format(**locals())))
 
 
 	plt.figure(1)
@@ -314,19 +320,19 @@ if __name__ == '__main__':
 	total_rewards_t = torch.tensor(total_rewards[0], dtype=torch.float)
 	plt.plot(smoothing_average(run_total_rewards[0]))
 
-	plt.subplot(413)
-	plt.xlabel('Episode')
-	plt.ylabel('Loss Classification')
-	loss_classification_t = torch.tensor(loss_classification[0], dtype=torch.float)
-	plt.plot(smoothing_average(run_loss_clf[0]))
+	
 
-	plt.subplot(414)
+	plt.subplot(413)
 	plt.xlabel('Episode')
 	plt.ylabel('max Q')
 	q_value_max = [np.max(q_values[i].cpu().detach().numpy(), axis=1) for i in range(NUM_EPISODES)]
-
 	plt.plot(smoothing_average(q_value_max))
-	plt.savefig(os.path.join(RESULT_DIR,'DQN_img_env_rank_OPT_CLF{OPTIMIZE_CLF}_{NUM_LABELS}labs_{RUNS}runs_{NUM_EPISODES}epis_{NUM_STEPS}steps_{WINDOW_SIZE}ws'.format(**locals())))
+
+	plt.subplot(414)
+	plt.xlabel('Episode')
+	plt.ylabel('Navig Loss from optimizer')
+	plt.plot(range(NUM_EPISODES-len(run_loss[0]), NUM_EPISODES), smoothing_average(run_loss[0]))
+	plt.savefig(os.path.join(RESULT_DIR,'annealingExp_combLoss_img_env_rank_{NUM_LABELS}labs_{RUNS}runs_{NUM_EPISODES}epis_{NUM_STEPS}steps_{WINDOW_SIZE}ws'.format(**locals())))
 	plt.show()
 		# sns.tsplot(data=smoothing_average(q_value_max), time=list(range(NUM_EPISODES)), ci=[68, 95], ax=plt.subplot(3, 1, 2), color='red')
 