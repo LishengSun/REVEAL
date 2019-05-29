2d218b2e7d0849cd64f65afcfb06e8387c4daeda
lishengsun <cecile829@gmail.com>
2019-05-29 17:55:45 +0200

    Added 2D Gaussian

--- agents/actor_critic/distributions.py
+++ agents/actor_critic/distributions.py
@@ -4,12 +4,15 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as F
 from utils import init, init_normc_, AddBias
+import pdb
 
 """
 Modify standard PyTorch distributions so they are compatible with this code.
 """
 
 FixedCategorical = torch.distributions.Categorical
+
+
 
 old_sample = FixedCategorical.sample
 FixedCategorical.sample = lambda self: old_sample(self).unsqueeze(-1)
@@ -22,6 +25,9 @@ FixedCategorical.mode = lambda self: self.probs.argmax(dim=1, keepdim=True)
 FixedNormal = torch.distributions.Normal
 log_prob_normal = FixedNormal.log_prob
 FixedNormal.log_probs = lambda self, actions: log_prob_normal(self, actions).sum(-1, keepdim=True)
+
+
+FixedMultivariateNormal = torch.distributions.MultivariateNormal
 
 entropy = FixedNormal.entropy
 FixedNormal.entropy = lambda self: entropy(self).sum(-1)
@@ -54,11 +60,11 @@ class DiagGaussian(nn.Module):
               lambda x: nn.init.constant_(x, 0))
 
         self.fc_mean = init_(nn.Linear(num_inputs, num_outputs))
+
         self.logstd = AddBias(torch.zeros(num_outputs))
 
     def forward(self, x):
         action_mean = self.fc_mean(x)
-
         #  An ugly hack for my KFAC implementation.
         zeros = torch.zeros(action_mean.size())
         if x.is_cuda:
@@ -66,3 +72,29 @@ class DiagGaussian(nn.Module):
 
         action_logstd = self.logstd(zeros)
         return x, FixedNormal(action_mean, action_logstd.exp())
+
+
+
+class TwoDGaussian(nn.Module):
+    def __init__(self, num_inputs, num_outputs):
+        super(TwoDGaussian, self).__init__()
+
+        init_ = lambda m: init(m,
+              init_normc_,
+              lambda x: nn.init.constant_(x, 0))
+
+        self.mean1 = init_(nn.Linear(num_inputs, num_outputs))
+        self.std1 = init_(nn.Linear(num_inputs, num_outputs))
+        self.mean2 = init_(nn.Linear(num_inputs, num_outputs))
+        self.std2 = init_(nn.Linear(num_inputs, num_outputs))
+        
+
+    def forward(self, x):
+        action_mean = torch.cat((self.mean1(x)[0], self.mean2(x)[0]))
+        # pdb.set_trace()
+        action_std = torch.eye(2)
+
+        action_std[0,0] = torch.clamp(self.std1(x), min= 1e-5)
+        action_std[1,1] = torch.clamp(self.std2(x), min= 1e-5)
+        return FixedMultivariateNormal(action_mean, action_std)
+        
